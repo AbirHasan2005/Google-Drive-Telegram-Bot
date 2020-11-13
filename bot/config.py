4f2b40259e52c62de7869958b09ef8b5e58dc527
Technical-lab <71844973+Technical-lab@users.noreply.github.com>
2020-11-13 16:08:25 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -1,7 +1,7 @@
 class config:
     BOT_TOKEN = "1419873726:AAGUKtR6ifvpp_0MKX2_hQYASXFgG7B-SA8"
-    APP_ID = ""
-    API_HASH = ""
+    APP_ID = "1917410"
+    API_HASH = "5291638b6743bbe8347a72999d72a938"
     DATABASE_URL = "postgres://wkzyonufulsuxt:9fd7165e1a0617d3d2119074f3790e41bae1d9f6d715a8307af0a0d26fa50c61@ec2-34-237-247-76.compute-1.amazonaws.com:5432/d3obkiqs0mjqpl"
     SUDO_USERS = "790744862" # Sepearted by space.
     SUPPORT_CHAT_LINK = "790744862"
