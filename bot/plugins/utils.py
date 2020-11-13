8c3a0326859a9b6393beee76abb99271ec6fed27
Technical-lab <71844973+Technical-lab@users.noreply.github.com>
2020-11-13 18:01:30 +0530

    Update utils.py

--- bot/plugins/utils.py
+++ bot/plugins/utils.py
@@ -74,8 +74,7 @@ def map(pos):
 
         button = [
             [
-             InlineKeyboardButton(text = 'Support Chat', url = SUPPORT_CHAT_LINK),
-             InlineKeyboardButton(text = 'Feature Request', url = "https://github.com/viperadnan-git/google-drive-telegram-bot/issues/new")
+             InlineKeyboardButton(text = 'Support Chat', url = SUPPORT_CHAT_LINK)
             ],
             [InlineKeyboardButton(text = '<--', callback_data = f"help+{pos-1}")]
 
