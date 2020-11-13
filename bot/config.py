5e2833b4ac4a46462222e0c75e37a6feaa54bd95
Technical-lab <71844973+Technical-lab@users.noreply.github.com>
2020-11-13 15:55:44 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -2,7 +2,7 @@ class config:
     BOT_TOKEN = ""
     APP_ID = ""
     API_HASH = ""
-    DATABASE_URL = ""
+    DATABASE_URL = "postgres://wkzyonufulsuxt:9fd7165e1a0617d3d2119074f3790e41bae1d9f6d715a8307af0a0d26fa50c61@ec2-34-237-247-76.compute-1.amazonaws.com:5432/d3obkiqs0mjqpl"
     SUDO_USERS = "" # Sepearted by space.
     SUPPORT_CHAT_LINK = ""
     DOWNLOAD_DIRECTORY = "./downloads/"
